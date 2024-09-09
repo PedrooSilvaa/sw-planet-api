05cde723b070e94a1ea35a369d02289a6b1b37a2
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-09 13:30:55 -0300

    feat: create PlanetControlle

---
+++ src/main/java/tech/silva/swplanetapi/controller/PlanetController.java
@@ -0,0 +1,9 @@
+package tech.silva.swplanetapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/planets")
+public class PlanetController {
+}
