a6169b189d4e5ff308dfaa742679e12ac7c26a97
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-09 13:33:25 -0300

    feat: create PlanetService

--- src/main/java/tech/silva/swplanetapi/controller/PlanetController.java
+++ src/main/java/tech/silva/swplanetapi/controller/PlanetController.java
@@ -1,9 +1,20 @@
 package tech.silva.swplanetapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.silva.swplanetapi.entity.Planet;
+import tech.silva.swplanetapi.service.PlanetService;
 
 @RestController
 @RequestMapping("/planets")
 public class PlanetController {
+
+    @Autowired
+    private PlanetService planetService;
+
+
 }
---
+++ src/main/java/tech/silva/swplanetapi/service/PlanetService.java
@@ -0,0 +1,9 @@
+package tech.silva.swplanetapi.service;
+
+import org.springframework.stereotype.Service;
+import tech.silva.swplanetapi.entity.Planet;
+
+@Service
+public class PlanetService {
+
+}
